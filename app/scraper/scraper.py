dea6ad00d4979895e080dc4616fcefc61dc91194
Jun Wei <jwpro637@gmail.com>
2026-06-15 02:22:10 +0800

    Added price scraping test on an example product

--- app/scraper/scraper.py
+++ app/scraper/scraper.py
@@ -1,10 +1,15 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-# test if selenium is working
-def scrape_test(url):
+# test if selenium is working, then scrape and format data
+def scrape_price(url):
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get(url)
+    time.sleep(10)
+    price = driver.find_element(By.CSS_SELECTOR, "div.IZPeQz.B67UQ0")
+    print(price.text)
     driver.quit()
-scrape_test("https://shopee.com.my")
+scrape_price("https://shopee.com.my/P9-Wireless-Bluetooth-Sports-Headphones-with-Mic-Noise-Reduction-On-Ear-Stereo-Headset-i.1206023866.24878743362?extraParams=%7B%22display_model_id%22%3A176458449017%2C%22model_selection_logic%22%3A3%7D")

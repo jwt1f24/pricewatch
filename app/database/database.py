a71453dcfacecdc3dc4e288f3965e3493b7c66c7
Jun Wei <jwpro637@gmail.com>
2026-06-15 19:04:48 +0800

    Added syntax for database connection and queries to create tables if it does not exist yet

--- app/database/database.py
+++ app/database/database.py
@@ -1,14 +1,48 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
+from pathlib import Path
+
+# locate .env file & return its contents to establish database connection
+load_dotenv(dotenv_path = Path(__file__).parents[2] / "credentials.env")
+conn = psycopg2.connect(
+    dbname = os.getenv("DBNAME"),
+    host = os.getenv("HOST"),
+    port = os.getenv("PORT"),
+    user = os.getenv("USER"),
+    password = os.getenv("PASSWD")
+)
+
+# use cursor to execute sql commands
+cursor = conn.cursor()
 
 def create_tables():
-    conn = psycopg2.connect(
-        dbname = os.getenv("DBNAME"),
-        host = os.getenv("HOST"),
-        port = os.getenv("PORT"),
-        user = os.getenv("USER"),
-        password = os.getenv("PASSWD")
+    # create products table
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS products ("
+        "product_id SERIAL PRIMARY KEY,"
+        "name VARCHAR(255) NOT NULL,"
+        "price DECIMAL(10, 2) NOT NULL,"
+        "url TEXT NOT NULL,"
+        "target_price DECIMAL(10, 2) NOT NULL,"
+        "email VARCHAR(255) NOT NULL,"
+        "stock INT NOT NULL"
+        ");"
     )
 
+    # create product history table
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS history ("
+        "history_id SERIAL PRIMARY KEY,"
+        "product_id INT REFERENCES products(product_id),"
+        "price DECIMAL(10, 2) NOT NULL,"
+        "date TIMESTAMP NOT NULL"
+        ");"
+    )
+
+    conn.commit()
+    cursor.close()
     conn.close()
+    print("Tables have been created.")
+
+create_tables()

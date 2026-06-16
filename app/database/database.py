bc9b3ec2ec3a120293dbcb6d026d4b5964711645
Jun Wei <jwpro637@gmail.com>
2026-06-16 15:11:33 +0800

    Added users table for authentiacation features

--- app/database/database.py
+++ app/database/database.py
@@ -19,13 +19,21 @@ def create_tables():
     conn = connect()
     cursor = conn.cursor() # cursor function allows sql command execution
     cursor.execute(
+        "CREATE TABLE IF NOT EXISTS users ("
+        "user_id SERIAL PRIMARY KEY,"
+        "email VARCHAR(255) NOT NULL,"
+        "password VARCHAR(255) NOT NULL,"
+        "date_created TIMESTAMP NOT NULL"
+        ");"
+    )
+    cursor.execute(
         "CREATE TABLE IF NOT EXISTS products ("
         "product_id SERIAL PRIMARY KEY,"
+        "user_id INT REFERENCES users(user_id),"
         "name VARCHAR(255) NOT NULL,"
-        "price DECIMAL(10, 2) NOT NULL,"
         "url TEXT NOT NULL,"
+        "current_price DECIMAL(10, 2) NOT NULL,"
         "target_price DECIMAL(10, 2) NOT NULL,"
-        "email VARCHAR(255) NOT NULL,"
         "stock INT NOT NULL"
         ");"
     )

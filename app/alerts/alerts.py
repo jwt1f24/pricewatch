8643a747ef18ee88b737446a11f700294ffd9bc9
Jun Wei <jwpro637@gmail.com>
2026-06-16 17:47:39 +0800

    Implemented email message formatting and conditionals to check the type of product change to alert

---
+++ app/alerts/alerts.py
@@ -0,0 +1,46 @@
+import smtplib
+from email.mime.text import MIMEText
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+# get admin credentials to send email
+load_dotenv(dotenv_path=Path(__file__).parents[2] / "credentials.env")
+sender = os.getenv("EMAIL")
+password = os.getenv("EMAIL_PW")
+
+def alert(alert_type, recipient, product, old_price=None, new_price=None):
+    # check the product's type of change to determine email message
+    if alert_type == "price_rise":
+        subject = "Alert! Product Price Raised"
+        body = (f"Hello!\n\nThe price of: '{product}' "
+                f"has increased from RM{old_price:.2f} to RM{new_price:.2f}."
+                f"\n\n- PriceWatch")
+    elif alert_type == "price_drop":
+        subject = "Alert! Product Price Dropped"
+        body = (f"Hello!\n\nThe price of: '{product}' "
+                f"has decreased from RM{old_price:.2f} to RM{new_price:.2f}."
+                f"\n\n- PriceWatch")
+    elif alert_type == "unavailable":
+        subject = "Alert! Product Out Of Stock"
+        body = (f"Hello!\n\nThe product: '{product}' is currently out of stock."
+                f"\n\n- PriceWatch")
+    elif alert_type == "available":
+        subject = "Alert! Product Back In Stock"
+        body = (f"Hello!\n\nThe product: '{product}' is now back in stock, "
+                f"at a price of RM{new_price:.2f}."
+                f"\n\n- PriceWatch")
+    else:
+        return
+
+    # message variables
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = sender
+    msg["To"] = recipient
+
+    # send email va smtplib server
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(msg["From"], password)
+        server.sendmail(msg["From"], msg["To"], msg.as_string())

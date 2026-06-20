6f5bfd455dfac3a1cb7d00e15731e1f2cfda7607
Jun Wei <jwpro637@gmail.com>
2026-06-20 14:46:47 +0800

    Added product price checking

--- app/scheduler/scheduler.py
+++ app/scheduler/scheduler.py
@@ -1,6 +1,7 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime
 from app.scraper.scraper import scrape_price
-from app.database.database import get_history, insert_history, insert_product
+from app.database.database import get_products, insert_history, update_product
 from app.alerts.alerts import alert
 
 # configure scheduling instance
@@ -8,8 +9,29 @@ sch = BlockingScheduler()
 
 # call the function for each time interval specified
 @sch.scheduled_job("interval",  hours=24)
-def check_price(old_price, new_price):
-    old = get_history(old_price)
-    new = scrape_price(new_price)
+def check_price():
+    products = get_products()
+    for product in products:
+        old_price = product["current_price"]
+        new = scrape_price(url=product["url"])
 
-sch.start()
+        # compare both prices, replace old price if new price is different
+        if old_price != new["price"] or product["stock"] != new["stock"]:
+            update_product(product["product_id"], new["price"], new["stock"])
+            insert_history(product["product_id"], new["price"], datetime.now())
+
+            # price condition
+            if new["price"] > old_price:
+                alert("price_rise", product["email"], product["name"], old_price, new["price"])
+            elif new["price"] < old_price:
+                alert("price_drop", product["email"], product["name"], old_price, new["price"])
+
+            # stock condition
+            if product["stock"] > 0 and new["stock"] <= 0:
+                alert("unavailable", product["email"], product["name"], old_price, new["price"])
+            elif product["stock"] <= 0 and new["stock"] > 0:
+                alert("available", product["email"], product["name"], old_price, new["price"])
+        else:
+            continue
+
+# sch.start()

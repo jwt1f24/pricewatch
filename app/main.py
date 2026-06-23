8f5a447e4b23929f6bea5d570b7a9a0e96565f88
Jun Wei <jwpro637@gmail.com>
2026-06-24 01:29:56 +0800

    Added user registration model and endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from app.scraper.scraper import scrape
-from app.database.database import insert_product, get_user_products, delete_product as db_del_product, get_history
+from app.database.database import insert_product, get_user_products, delete_product as db_del_product, get_history, insert_user
+from datetime import datetime
+import bcrypt
 
 app = FastAPI()
 
@@ -10,10 +12,24 @@ app = FastAPI()
 def root():
     return {"message": "PriceWatch API"}
 
+# user input endpoint
 class ProductRequest(BaseModel):
     user_id: int
     url: str
     target_price: float
+
+# register model
+class UserRequest(BaseModel):
+    email: str
+    password: str
+
+# register endpoint
+@app.post("/register")
+def register(request: UserRequest):
+    bcrypt.hashpw(UserRequest.password.encode('utf-8'), bcrypt.gensalt())
+    insert_user(request.email, request.password, datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    return {"message": "User registered successfully"}
+
 
 # track product price & store data into database
 @app.post("/products")
@@ -37,8 +53,3 @@ def delete_product(product_id: int):
 @app.get("/products/{product_id}/history")
 def history_product(product_id: int):
     return {"history": get_history(product_id)}
-
-# register endpoint
-class UserRequest(BaseModel):
-    email: str
-    password: str

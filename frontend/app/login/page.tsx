ebf99fc204190c4e8ad136b0942ea89feccad209
Jun Wei <jwpro637@gmail.com>
2026-06-27 23:42:33 +0800

    Add login error handling

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,10 +1,33 @@
 "use client"; // login page state instance
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 // primary function - handle user login
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  // call API upon button click
+  async function handleLogin() {
+    console.log("Login button clicked");
+
+    const apicall = await fetch("http://localhost:8000/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    const data = await apicall.json();
+
+    // error handling, save token & redirect if login successful
+    if (!data.token) {
+      setError("Incorrect credentials");
+    } else {
+      localStorage.setItem("token", data.token);
+      router.push("/dashboard");
+    }
+  }
 
   return (
     <main>
@@ -20,11 +43,8 @@ export default function Login() {
         onChange={(e) => setPassword(e.target.value)}
         placeholder="Enter password"
       />
-      <input type="button" onClick={HandleLogin} value={"submit"}>
-        Log In
-      </input>
+      <button onClick={handleLogin}>Log In</button>
+      {error && <p>{error}</p>}
     </main>
   );
 }
-
-function HandleLogin() {}
